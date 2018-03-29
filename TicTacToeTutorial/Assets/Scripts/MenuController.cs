a4da2c837a50e37948667d088a4849cd837fd7c7
WilliCheung <william.cheung1994@gmail.com>
2018-03-29 12:06:09 +0200

    La in ett menu script

---
+++ TicTacToeTutorial/Assets/Scripts/MenuController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController: MonoBehaviour {
+
+	public static MenuController menuController;
+
+	private void Start()
+	{
+		menuController = this;
+	}
+
+	public void ToPlayScene()
+	{
+		SceneManager.LoadScene("Menu");
+	}
+}
