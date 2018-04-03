645a9d48773c13ddf2add8abb911951ed9e22981
teoalagic <teoalagicenglund@hotmail.com>
2018-04-03 18:50:55 +0200

    ui

--- TicTacToeTutorial/Assets/Scripts/MenuController.cs
+++ TicTacToeTutorial/Assets/Scripts/MenuController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+
 
 
 public class MenuController: MonoBehaviour {
